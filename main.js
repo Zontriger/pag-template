d097d657974c962fe751938df6ba59267b5b2186
zontriger <zontriger@gmail.com>
2025-09-01 03:59:24 -0400

    Primera versión completa de plantilla de página principal

---
+++ main.js
@@ -0,0 +1,29 @@
+const menuBtnNav = document.querySelector("nav > .menu-btn");
+const hideBtnSidebar = document.querySelector("#sidebar > .hide-wrapper > .hide-btn");
+const sidebar = document.querySelector("#sidebar");
+const sidebarOverlay = document.querySelector("#sidebar-overlay");
+const footer = document.querySelector("footer");
+
+// Muestra el sidebar para móviles
+const showMobileSidebar = () => {
+	document.body.classList.add("no-scroll");
+	sidebarOverlay.classList.add("show");
+	sidebar.classList.add("show");
+}
+
+// Oculta el sidebar para móviles
+const hideMobileSidebar = () => {
+	document.body.classList.remove("no-scroll");
+	sidebarOverlay.classList.remove("show");
+	sidebar.classList.remove("show");
+}
+
+// Muestra u oculta el sidebar para escritorio
+const toggleSidebarHide = () => {
+	hideBtnSidebar.classList.toggle("on");
+	sidebar.classList.toggle("offscreen");
+}
+
+hideBtnSidebar.addEventListener("click", toggleSidebarHide);
+menuBtnNav.addEventListener("click", showMobileSidebar);
+sidebarOverlay.addEventListener("click", hideMobileSidebar);
